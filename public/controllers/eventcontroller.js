2056df2e7b695ec6c1a86cbc0cf7861b613bcf91
Rami <117773986+RamiNoodle733@users.noreply.github.com>
2024-11-01 16:10:47 -0500

    Update eventcontroller.js

--- public/controllers/eventcontroller.js
+++ public/controllers/eventcontroller.js
@@ -22,7 +22,7 @@ exports.getEvents = async (req, res) => {
 exports.getEventById = async (req, res) => {
   try {
     const event = await Event.findById(req.params.id);
-    if (!event) return res.status(404).json({ status: 'error', message: 'Event not found' });
+    if not (event) return res.status(404).json({ status: 'error', message: 'Event not found' });
     res.status(200).json(event)
   } catch (error) {
     res.status(400).json({ status: 'error', message: error.message });
