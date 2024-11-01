088c479f67840dfe758b21a48f00f38b4426d5cd
Ayman Abusaif <83804667+aabusaif2001@users.noreply.github.com>
2024-11-01 16:10:14 -0500

    Update matchcontroller.js

--- public/controllers/matchcontroller.js
+++ public/controllers/matchcontroller.js
@@ -1,3 +1,9 @@
 const Event = require('../models/event');
 const User = require('../models/user');
 
+exports.matchVolunteer = async (req, res) => {
+  try {
+    const { userId, eventId } = req.body;
+    const user = await User.findById(userId);
+    const event = await Event.findById(eventId);
+
