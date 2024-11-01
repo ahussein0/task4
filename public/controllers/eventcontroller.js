f29c18356db1f4a717b5e19ec572e6d61ab7c832
Rami <117773986+RamiNoodle733@users.noreply.github.com>
2024-11-01 15:55:23 -0500

    Update eventcontroller.js

--- public/controllers/eventcontroller.js
+++ public/controllers/eventcontroller.js
@@ -1,1 +1,9 @@
 const Event = require('../models/event');
+
+exports.createEvent = async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.status(201).json({ status: 'success', message: 'Event created successfully' });
+  }
+};
